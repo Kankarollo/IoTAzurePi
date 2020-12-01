9eee516da2301b235e2fa8b00e1ded5d50030b75
Igor Marzynski <igormarzynski@gmail.com>
2020-12-02 00:41:23 +0100

    Upgrade filtering + chart drawing

--- WebApp/controllers/measures.js
+++ WebApp/controllers/measures.js
@@ -1,12 +1,8 @@
 const iotData = require("../models/iotDataModel");
 
 exports.post_get_measures = function (req, res) {
-    bodyFilter = {};
-    if (req.body.deviceId != '') {
-        bodyFilter = { DeviceId: req.body.deviceId };
-    }
     (async () => {
-        let measures = await iotData.find(bodyFilter).exec();
+        let measures = await iotData.find(req.body).exec();
         res.json(measures);
     })().catch();
 };
--- WebApp/public/js/analytics.js
+++ WebApp/public/js/analytics.js
@@ -1,51 +1,99 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
-async function getCollectedData(deviceId) {
+async function getCollectedData(filterBody) {
     const response = await fetch('/measures', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(deviceId)
+        body: JSON.stringify(filterBody)
     });
     return response.json();
+}
+
+function prepareFilterBody(deviceID, startDate, endDate) {
+    var filterBody = {};
+    if (deviceID != '') {
+        var allDevicesID = deviceID.split(",");
+        filterBody['DeviceId'] = allDevicesID;
+    }
+    if (startDate != '') {
+        var tmp = new Date(startDate).toISOString();
+        filterBody['MessageDate'] = { $gte: tmp };
+    }
+    if (endDate != '') {
+        var tmp = new Date(endDate).toISOString();
+        if (filterBody['MessageDate']['$gte']) {
+            filterBody['MessageDate']['$lte'] = tmp;
+        } else {
+            filterBody['MessageDate'] = { $lte: tmp };
+        }
+    }
+    return filterBody;
+}
+
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
 
 $(document).ready(() => {
     const button = document.getElementById('filterButton');
 
     button.addEventListener('click', function (e) {
-        const deviceIdInput = document.getElementById('deviceName');
-        const measurementTypeInput = document.getElementById('measurementType');
+        const deviceIdInput = document.getElementById('deviceName').value;
+        const startDateInput = document.getElementById("startDate").value;
+        const endDateInput = document.getElementById("endDate").value;
+
+        const measurementTypeInput = document.getElementById('measurementType').value;
 
         var ctx = document.getElementById('analyticsChart').getContext('2d');
 
-        data = [];
-        labels = [];
+        var labels = [];
+        var chartData = {};
 
-        getCollectedData({ deviceId: deviceIdInput.value }).then(measures => {
-            console.log(measures);
+        const filterBody = prepareFilterBody(deviceIdInput, startDateInput, endDateInput);
+
+        getCollectedData(filterBody).then(measures => {
             measures.forEach(element => {
-                data.push(element.IotData[measurementTypeInput.value]);
+                if (!chartData[element.DeviceId]) {
+                    chartData[element.DeviceId] = [{
+                        x: element.MessageDate,
+                        y: element.IotData[measurementTypeInput]
+                    }];
+                } else {
+                    chartData[element.DeviceId].push({
+                        x: element.MessageDate,
+                        y: element.IotData[measurementTypeInput]
+                    });
+                }
                 labels.push(element.MessageDate);
             });
-
+            console.log(chartData);
+            console.log(Object.keys(chartData));
+            var devices = Object.keys(chartData);
             var chart = new Chart(ctx, {
                 type: 'line',
-
                 data: {
-                    labels: labels,
-                    datasets: [{
-                        fill: false,
-                        label: deviceIdInput.value,
-                        backgroundColor: 'rgba(255, 204, 0, 0.4)',
-                        borderColor: 'rgba(255, 204, 0, 1)',
-                        data: data
-                    }]
-                },
+                    labels: labels
+                }
+            });
 
-                options: {}
+            console.log(chartData["SimulatorDevice"]);
+            devices.forEach(device => {
+                const color = getRandomRgb();
+                chart.data.datasets.push({
+                    label: device,
+                    data: chartData[device],
+                    fill: false,
+                    backgroundColor: color,
+                    borderColor: color
+                });
+                chart.update();
             });
         });
 
